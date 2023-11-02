5c71d1202ec1fa06b70304cbfba3fa5865517199
Hyomk <qet557@gmail.com>
2023-11-03 01:58:08 +0900

    [del] 불필요 코드 삭제

--- app/src/main/java/com/ssu/better/ui/theme/Type.kt
+++ app/src/main/java/com/ssu/better/ui/theme/Type.kt
@@ -1,6 +1,5 @@
 package com.ssu.better.ui.theme
 
-import androidx.compose.material3.Typography
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
@@ -93,86 +92,6 @@ val Typography = BetterTypography(
         letterSpacing = 0.sp,
     ),
 
-)
-
-val Title = TextStyle(
-    fontFamily = pretendard,
-    fontWeight = FontWeight.Medium,
-    fontSize = 20.sp,
-    lineHeight = 24.sp,
-    letterSpacing = 0.4.sp,
-)
-
-val Heading0 = TextStyle(
-    fontFamily = pretendard,
-    fontWeight = FontWeight.Medium,
-    fontSize = 36.sp,
-    lineHeight = 43.sp,
-    letterSpacing = 0.5.sp,
-)
-
-val Heading1 = TextStyle(
-    fontFamily = pretendard,
-    fontWeight = FontWeight.Medium,
-    fontSize = 18.sp,
-    lineHeight = 21.5.sp,
-    letterSpacing = 0.sp,
-)
-
-val Heading2 = TextStyle(
-    fontFamily = pretendard,
-    fontWeight = FontWeight.Medium,
-    fontSize = 16.sp,
-    lineHeight = 19.sp,
-    letterSpacing = 0.sp,
-)
-
-val Heading3 = TextStyle(
-    fontFamily = pretendard,
-    fontWeight = FontWeight.Medium,
-    fontSize = 14.sp,
-    lineHeight = 16.7.sp,
-    letterSpacing = 0.sp,
-)
-
-val Heading4 = TextStyle(
-    fontFamily = pretendard,
-    fontWeight = FontWeight.Medium,
-    fontSize = 12.sp,
-    lineHeight = 14.3.sp,
-    letterSpacing = 0.sp,
-)
-
-val Subtitle = TextStyle(
-    fontFamily = pretendard,
-    fontWeight = FontWeight.Normal,
-    fontSize = 14.sp,
-    lineHeight = 16.7.sp,
-    letterSpacing = 0.sp,
-)
-
-val Body = TextStyle(
-    fontFamily = pretendard,
-    fontWeight = FontWeight.Normal,
-    fontSize = 12.sp,
-    lineHeight = 14.3.sp,
-    letterSpacing = 0.sp,
-)
-
-val Caption = TextStyle(
-    fontFamily = pretendard,
-    fontWeight = FontWeight.Normal,
-    fontSize = 12.sp,
-    lineHeight = 14.3.sp,
-    letterSpacing = 0.sp,
-)
-
-val Option = TextStyle(
-    fontFamily = FontFamily.Default,
-    fontWeight = FontWeight.Normal,
-    fontSize = 10.sp,
-    lineHeight = 11.9.sp,
-    letterSpacing = 0.sp,
 )
 
 @Immutable
