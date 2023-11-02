5eb22d2269d1e036931957da231ea6df8f6a829f
Hyomk <qet557@gmail.com>
2023-11-03 01:55:17 +0900

    [feat] font 추가

--- app/src/main/java/com/ssu/better/ui/theme/Type.kt
+++ app/src/main/java/com/ssu/better/ui/theme/Type.kt
@@ -3,7 +3,9 @@ package com.ssu.better.ui.theme
 import androidx.compose.material3.Typography
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.ssu.better.R
