d5d7f4fbf44a9eb1ab80a08a5623404367b33ed4
Hyomk <qet557@gmail.com>
2023-11-07 15:56:08 +0900

    [feat] root graph 설계

---
+++ app/src/main/java/com/ssu/better/presentation/navigation/RootNavGraph.kt
@@ -0,0 +1,19 @@
+package com.ssu.better.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.ssu.better.ui.theme.main.MainScreen
+
+@Composable
+fun RootNavGraph(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Main.Home.route,
+    ) {
+        composable(route = Screen.Main.Home.route) {
+            MainScreen()
+        }
+    }
+}
---
+++ app/src/main/java/com/ssu/better/presentation/navigation/Screen.kt
@@ -0,0 +1,14 @@
+package com.ssu.better.presentation.navigation
+
+sealed class Screen(val route: String) {
+    object Splash : Screen("splash")
+    object Login : Screen("login")
+    object OnBoard : Screen("onboard")
+    object Main : Screen("main") {
+        object Home : Screen("home")
+        object Search : Screen("search")
+        object MyPage : Screen("mypage")
+
+        object Sample : Screen("sample")
+    }
+}
