279798bd5e6391b00b40ee18daaaff770df2d4aa
Hyomk <qet557@gmail.com>
2023-11-19 02:56:22 +0900

    [fix] Api 변경 내용 반영

--- domain/src/main/java/com/ssu/better/domain/repository/UserRepository.kt
+++ domain/src/main/java/com/ssu/better/domain/repository/UserRepository.kt
@@ -3,7 +3,7 @@ package com.ssu.better.domain.repository
 import com.ssu.better.entity.challenge.Challenge
 import com.ssu.better.entity.task.Task
 import com.ssu.better.entity.user.User
-import com.ssu.better.entity.user.UserLoginRequest
+import com.ssu.better.entity.user.UserCheck
 import com.ssu.better.entity.user.UserLoginResponse
 import com.ssu.better.entity.user.UserRank
 import com.ssu.better.entity.user.UserRegisterRequest
@@ -12,7 +12,7 @@ import kotlinx.coroutines.flow.Flow
 interface UserRepository {
     suspend fun registerUser(userRegisterRequest: UserRegisterRequest): Flow<User>
 
-    suspend fun login(userLoginRequest: UserLoginRequest): Flow<UserLoginResponse>
+    suspend fun login(kakaoToken: String): Flow<UserLoginResponse>
 
     suspend fun getUser(userId: Long): Flow<User>
 
@@ -21,4 +21,6 @@ interface UserRepository {
     suspend fun getUserTasks(userId: Long): Flow<List<Task>>
 
     suspend fun getUserChallenges(userId: Long): Flow<List<Challenge>>
+
+    suspend fun getUserCheck(userId: Long): Flow<UserCheck>
 }
--- domain/src/main/java/com/ssu/better/domain/usecase/user/PostUserLoginRequestUseCase.kt
+++ domain/src/main/java/com/ssu/better/domain/usecase/user/PostUserLoginRequestUseCase.kt
@@ -1,11 +1,12 @@
 package com.ssu.better.domain.usecase.user
 
 import com.ssu.better.domain.repository.UserRepository
-import com.ssu.better.entity.user.UserLoginRequest
 
 class PostUserLoginRequestUseCase(
     private val repository: UserRepository,
 ) {
-    suspend fun login(userLoginRequest: UserLoginRequest) =
-        repository.login(userLoginRequest)
+    suspend fun login(kakaoToken: String) =
+        repository.login(kakaoToken)
+
+    suspend fun userCheck(userId: Long) = repository.getUserCheck(userId)
 }
