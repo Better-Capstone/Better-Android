002200966bffbeddbd2a39f8bafb828074764639
Harry(배현빈) <baehynebin@hotmail.co.kr>
2023-11-23 17:49:18 +0900

    [imporve] lint 오류 수정

--- domain/src/main/java/com/ssu/better/domain/usecase/study/GetStudyTaskListUseCase.kt
+++ domain/src/main/java/com/ssu/better/domain/usecase/study/GetStudyTaskListUseCase.kt
@@ -3,7 +3,7 @@ package com.ssu.better.domain.usecase.study
 import com.ssu.better.domain.repository.StudyRepository
 
 class GetStudyTaskListUseCase(
-    private val studyRepository: StudyRepository
+    private val studyRepository: StudyRepository,
 ) {
     suspend fun getStudyTaskList(studyId: Long) =
         studyRepository.getStudyTaskList(studyId)
