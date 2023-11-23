8feb1c67400ec29c087755217d04f300fcad7dc6
Hyomk <qet557@gmail.com>
2023-11-24 01:02:49 +0900

    [feat] 스터디 검색 API 추가

--- app/src/main/java/com/ssu/better/di/modules/UseCaseModule.kt
+++ app/src/main/java/com/ssu/better/di/modules/UseCaseModule.kt
@@ -4,6 +4,7 @@ import com.ssu.better.domain.repository.StudyRepository
 import com.ssu.better.domain.repository.UserRepository
 import com.ssu.better.domain.usecase.study.GetGroupRankHistoryUseCase
 import com.ssu.better.domain.usecase.study.GetStudyListByCategoryUseCase
+import com.ssu.better.domain.usecase.study.GetStudyListByQueryUseCase
 import com.ssu.better.domain.usecase.study.GetStudyListByUserUseCase
 import com.ssu.better.domain.usecase.study.GetStudyListUseCase
 import com.ssu.better.domain.usecase.study.GetStudyUseCase
@@ -107,5 +108,11 @@ object UseCaseModule {
     @Singleton
     fun providesGetStudyUserListUseCase(repository: StudyRepository): GetStudyUserListUseCase {
         return GetStudyUserListUseCase(repository)
+    }
+
+    @Provides
+    @Singleton
+    fun providesGetStudyListByQueryUseCase(repository: StudyRepository): GetStudyListByQueryUseCase {
+        return GetStudyListByQueryUseCase(repository)
     }
 }
