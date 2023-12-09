312eec3b2a9149a599156d639b275871c86d34bb
coding-polarbear <baehynebin@hotmail.co.kr>
2023-12-10 02:43:10 +0900

    [improve] 버튼 부 화면 짤림 문제 해결 (Vertical Scroll 추가)

--- app/src/main/java/com/ssu/better/presentation/ui/challenge/approve/ChallengeApproveScreen.kt
+++ app/src/main/java/com/ssu/better/presentation/ui/challenge/approve/ChallengeApproveScreen.kt
@@ -11,7 +11,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -207,7 +209,8 @@ fun ChallengeApproveContent(
                 Column(
                     modifier = Modifier
                         .padding(paddingValues)
-                        .background(BetterColors.Bg),
+                        .background(BetterColors.Bg)
+                        .verticalScroll(rememberScrollState()),
                 ) {
                     Text(
                         modifier = Modifier.padding(start = 20.dp, top = 30.dp),
