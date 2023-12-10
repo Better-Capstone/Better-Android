953f4b4cca35f7fd9d67e2232a5955ede595d75f
Hyomk <qet557@gmail.com>
2023-12-10 21:59:01 +0900

    [release] Icon 등록

--- app/src/main/java/com/ssu/better/presentation/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/ssu/better/presentation/ui/login/LoginViewModel.kt
@@ -49,7 +49,7 @@ class LoginViewModel @Inject constructor(
             if (UserApiClient.instance.isKakaoTalkLoginAvailable(context)) {
                 UserApiClient.instance.loginWithKakaoTalk(context) { token, error ->
                     if (error != null) {
-                        Timber.e("카카오톡으로 로그인 실패", error)
+                        Timber.e("카카오톡으로 로그인 실패" + error.message)
 
                         // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
                         // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
